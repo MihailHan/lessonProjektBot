6c2a90ae2d089e5e160cdd950a9b7000073eb389
Михаил <MKX0506@mail.ru>
2024-02-22 16:27:26 +0300

    отправка сообщения всем пользователям

--- main.ru.py
+++ main.ru.py
@@ -1,18 +1,38 @@
 import requests
 import pprint
-
 with open('token.txt') as f:
     token = f.read()
 
-# endPoint = f'https://api.telegram.org/bot{token}/getMe'
-# res = requests.get(endPoint).json()
-# pprint.pprint(res)
+import time
+while True:
+
+
 
 endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-res = requests.get(endPoint).json()
-pprint.pprint(res)
-chatID = res['result'][0]['message']['chat']['id']
-print(chatID)
+response = requests.get(endPoint).json()['result']
+pprint.pprint(response)
+
+
+mes = f'Привет, {response[0]['message']['from']['first_name']}!'
+chatID = response[0]['message']['chat']['id']
+usersInfo = dict()
+for i in response:
+    chatID = i['message']['chat']['id']
+    userName = i['message']['chat']['first_name']
+    if 'text' in i['message']:
+        userText = i['message']['text']
+    usersInfo[chatID] = [userName, userText]
+
+
+print(usersInfo)
+
 endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-params = {'chat_id':chatID, 'text': "Приветствую вас!"}
-res = requests.get(endPoint, params=params).json()
+params = {'chat_id': chatID, 'text': mes}
+response = requests.get(endPoint, params=params)
+for user in usersInfo:
+    mes = f'Привет, {usersInfo[user][0]}!'
+    params = {'chat_id': user, 'text': mes}
+    res = requests.get(endPoint, params=params)
+
+
+
