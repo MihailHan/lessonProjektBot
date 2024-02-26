2741987a02ec46cfa6e17be747c89a6fa42a3c1c
Михаил <MKX0506@mail.ru>
2024-02-26 19:45:31 +0300

    Астрономическое фото без проверки на дату

--- main.ru.py
+++ main.ru.py
@@ -11,8 +11,27 @@ with open('token.txt') as f:
 # pprint.pprint(response)
 
 
+#Функция, дает косм.фото по дате
+def givePhoto(date: str) -> (str,str):
+    endPoint = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': 'DEMO_KEY', 'date': date}
+    res = requests.get(endPoint, params=params).json()
+    explanation = res['explanation']
+    urlPhoto = res['url']
+    return (urlPhoto,explanation)
 
-#Бесконечные ответы
+
+#Заготовка для бота с функцией
+# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+# res = requests.get(endPoint).json()
+# chatID = res['result'][0]['message']['chat']['id']
+# endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+# params = {'chat_id':chatID, 'photo':givePhoto('2024-01-24')[0]}
+# response = requests.get(endPoint, params=params).json()
+
+
+
+#Бесконечные ответы (в последствии эхо - бот)
 offset = -2
 while True:
     endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
@@ -20,14 +39,21 @@ while True:
     response = requests.get(endPoint, params=params).json()
     if response['result']:
         offset = response['result'][0]['update_id']
-        userText = response['result'][0]['message']['text']
         chatID = response['result'][0]['message']['chat']['id']
+        userText = response['result'][0]['message']['text']
+        photoURL, photoEXP = givePhoto(userText)
+        endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+        params = {'chat_id':chatID, 'photo':photoURL}
+        response = requests.get(endPoint, params=params)
         endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-        params = {'chat_id': chatID, 'text': userText}
-        res = requests.get(endPoint, params=params)
+        params = {'chat_id': chatID, 'text':photoEXP}
+        response = requests.get(endPoint, params=params)
     time.sleep(1)
 
 
+
+
+#Отправка еденичного сообщения (не автомат)
 # endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
 # response = requests.get(endPoint).json()['result']
 # pprint.pprint(response)
@@ -50,5 +76,29 @@ while True:
 #     mes = f'Привет, {usersInfo[user][0]}!'
 #     params = {'chat_id': user, 'text': mes}
 # response = requests.get(endPoint, params=params)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
