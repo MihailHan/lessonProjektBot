8745e6df15b94b529f72007abb8eadffd4e328dd
Михаил <MKX0506@mail.ru>
2024-02-26 17:29:28 +0300

    Первый "Эхо" - бот

--- main.ru.py
+++ main.ru.py
@@ -1,43 +1,54 @@
 import requests
 import pprint
+import time
 with open('token.txt') as f:
     token = f.read()
 
-# import time
-# while True:
-#     #получить информацию по всем событиям (апдейтам)
-#     endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-#     response = requests.get(endPoint).json()['result']
-#     userText = response[0]['message']['text']
-#     chatID = response[0]['message']['chat']['id']
-#     #pprint.pprint(response)
-#     endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-#     params = {'chat_id': chatID, 'text': userText}
-#     res = requests.get(endPoint, params=params)
-#     time.sleep(1)
-
-
-endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-response = requests.get(endPoint).json()['result']
-pprint.pprint(response)
+# #Оставляет только последние изменения(сообщения)
+# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+# params = {'timeout' : 60, 'offset': 0}
+# response = requests.get(endPoint, params=params).json()['result']
+# pprint.pprint(response)
 
 
 
-usersInfo = dict()
-for i in response:
-    chatID = i['message']['chat']['id']
-    userName = i['message']['chat']['first_name']
-    if 'text' in i['message']:
-        userText = i['message']['text']
-    usersInfo[chatID] = [userName, userText]
+#Бесконечные ответы
+offset = -2
+while True:
+    endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+    params = {'timeout': 60, 'offset': offset +1}
+    response = requests.get(endPoint, params=params).json()
+    if response['result']:
+        offset = response['result'][0]['update_id']
+        userText = response['result'][0]['message']['text']
+        chatID = response['result'][0]['message']['chat']['id']
+        endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+        params = {'chat_id': chatID, 'text': userText}
+        res = requests.get(endPoint, params=params)
+    time.sleep(1)
 
 
-print(usersInfo)
-
-endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-for user in usersInfo:
-    mes = f'Привет, {usersInfo[user][0]}!'
-    params = {'chat_id': user, 'text': mes}
-response = requests.get(endPoint, params=params)
+# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+# response = requests.get(endPoint).json()['result']
+# pprint.pprint(response)
+#
+#
+#
+# usersInfo = dict()
+# for i in response:
+#     chatID = i['message']['chat']['id']
+#     userName = i['message']['chat']['first_name']
+#     if 'text' in i['message']:
+#         userText = i['message']['text']
+#     usersInfo[chatID] = [userName, userText]
+#
+#
+# print(usersInfo)
+#
+# endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+# for user in usersInfo:
+#     mes = f'Привет, {usersInfo[user][0]}!'
+#     params = {'chat_id': user, 'text': mes}
+# response = requests.get(endPoint, params=params)
 
 
