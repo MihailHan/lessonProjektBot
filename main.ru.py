bc00768058796b2ee97ca7c3ed19a224617ffd5d
Михаил <MKX0506@mail.ru>
2024-02-29 17:10:17 +0300

    yes/no бот (выдает гифку)

--- main.ru.py
+++ main.ru.py
@@ -20,14 +20,11 @@ def givePhoto(date: str) -> (str,str):
     urlPhoto = res['url']
     return (urlPhoto,explanation)
 
-
-#Заготовка для бота с функцией
-# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-# res = requests.get(endPoint).json()
-# chatID = res['result'][0]['message']['chat']['id']
-# endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
-# params = {'chat_id':chatID, 'photo':givePhoto('2024-01-24')[0]}
-# response = requests.get(endPoint, params=params).json()
+#Функция дает гифку с ответом yes или no
+def giveAnimation(answer: str) -> str:
+    endPoint = f'https://yesno.wtf/api?force={answer}'
+    res = requests.get(endPoint).json()
+    return res['image']
 
 #Функция для проверки даты
 def checkDate(userData: str) -> bool:
@@ -56,46 +53,30 @@ while True:
         offset = response['result'][0]['update_id']
         chatID = response['result'][0]['message']['chat']['id']
         userText = response['result'][0]['message']['text']
-        if checkDate(userText):
-            photoURL, photoEXP = givePhoto(userText)
-            endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
-            params = {'chat_id':chatID, 'photo':photoURL}
-            response = requests.get(endPoint, params=params)
-            endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-            params = {'chat_id': chatID, 'text':photoEXP}
-            response = requests.get(endPoint, params=params)
-        else:
-            endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-            params = {'chat_id': chatID, 'text': 'Дата введена в неправильном формате!! Правильный формат: ГГГГ-ММ-ДД'}
-            response = requests.get(endPoint, params=params)
+        # if checkDate(userText):
+        #     photoURL, photoEXP = givePhoto(userText)
+        #     endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+        #     params = {'chat_id':chatID, 'photo':photoURL}
+        #     response = requests.get(endPoint, params=params)
+        #     endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+        #     params = {'chat_id': chatID, 'text':photoEXP}
+        #     response = requests.get(endPoint, params=params)
+        # else:
+        #     endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+        #     params = {'chat_id': chatID, 'text': 'Дата введена в неправильном формате!! Правильный формат: ГГГГ-ММ-ДД'}
+        #     response = requests.get(endPoint, params=params)
+        endPoint = f'https://api.telegram.org/bot{token}/sendAnimation'
+        params = {'chat_id':chatID, 'animation': giveAnimation(userText)}
+        response = requests.get(endPoint, params=params)
     time.sleep(1)
 
 
 
 
-#Отправка еденичного сообщения (не автомат)
-# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-# response = requests.get(endPoint).json()['result']
-# pprint.pprint(response)
-#
-#
-#
-# usersInfo = dict()
-# for i in response:
-#     chatID = i['message']['chat']['id']
-#     userName = i['message']['chat']['first_name']
-#     if 'text' in i['message']:
-#         userText = i['message']['text']
-#     usersInfo[chatID] = [userName, userText]
-#
-#
-# print(usersInfo)
-#
-# endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-# for user in usersInfo:
-#     mes = f'Привет, {usersInfo[user][0]}!'
-#     params = {'chat_id': user, 'text': mes}
-# response = requests.get(endPoint, params=params)
+
+
+
+
 
 
 
