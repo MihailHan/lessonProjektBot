ac008edfe87d7d98a366a2da12ba796c4cb377be
Михаил <MKX0506@mail.ru>
2024-03-07 19:25:23 +0300

    Добавлена клавиатура

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,4 +1,7 @@
-# key1 = KeyboardButton(text='Да!')
-# key2 = KeyboardButton(text='Нет')
-#
-# keyboard = ReplyKeyboardMarkup(keyboard=[[key1,key2]])
+from aiogram import types
+from lexicon.lexicon import LEXICON_RU
+
+yes_key = types.KeyboardButton(text=LEXICON_RU['yes_button'])
+no_key = types.KeyboardButton(text=LEXICON_RU['no_button'])
+
+yes_no_kb = types.ReplyKeyboardMarkup(keyboard=[[yes_key,no_key]])
