0d95256cf8821a5060480b6e3da129bff156d043
Михаил <MKX0506@mail.ru>
2024-03-21 16:36:45 +0300

    Переделаны команды, убрана клавиатура

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,6 @@ from aiogram.types import Message
 from aiogram.filters import Command,CommandStart
 from aiogram import Router,F
 from lexicon.lexicon import LEXICON_RU
-from keyboards.keyboards import yes_no_kb
 from services.card_deck import Game
 from sqlite3 import Connection
 
@@ -16,12 +15,12 @@ async def process_start_command(message: Message, dbConnect: Connection):
     cursor.execute('INSERT OR IGNORE INTO Users (tgid, username, allgames, wingames) VALUES (?, ?, ?, ?)',
                    (message.from_user.id, message.from_user.full_name, 0, 0))
     dbConnect.commit()
-    await message.answer(LEXICON_RU['/start'],reply_markup=yes_no_kb)
+    await message.answer(LEXICON_RU['start'])
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer(LEXICON_RU['/help'])
+    await message.answer(LEXICON_RU['help'])
 
 
 @router.message(Command(commands=['shuffle']))
